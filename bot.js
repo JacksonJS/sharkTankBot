4762916a6ec637d2e95cfb652038b44318fab15c
jacksonjs <jacksonjs@openmailbox.org>
2016-05-23 21:14:06 -0400

    removed forever monitor portion of script

--- bot.js
+++ bot.js
@@ -1,20 +1,6 @@
 // Debug flag
 
 var debug = false;
-/*var forever = require('forever-monitor');
-
-  var child = new (forever.monitor)('bot.js', {
-  max: 3,
-  silent: true,
-  args: []
-  });
-
-  child.on('exit', function () {
-  console.log('bot.js has exited after 3 restarts');
-  });
-
-  child.start();
-  */
 // Our Twitter library
 var Twit = require('twit');
 
